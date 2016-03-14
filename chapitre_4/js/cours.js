be45288d6194a3aed0b8bfcf8d354b7727702fa2
bpesquet <bpesquet@gmail.com>
2016-03-14 14:49:15 +0100

    Chapitre 4

--- chapitre_4/js/cours.js
+++ chapitre_4/js/cours.js
@@ -1,2 +1,14 @@
+var p1 = document.querySelector("p");
+p1.style.color = "red";
+p1.style.margin = "50px";
+p1.style.fontFamily = "Arial";
+p1.style.backgroundColor = "black";
+
+var paragraphes = document.getElementsByTagName("p");
+for (var i = 0; i < paragraphes.length; i++) {
+    console.log(paragraphes[i].style.color);
+}
+
 var style = getComputedStyle(document.getElementById("para"));
 console.log(style.color);
+console.log(style.fontStyle);
