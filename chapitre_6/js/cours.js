1fc6b9053415df0bc76d0d7cf51e810076b92453
bpesquet <bpesquet@gmail.com>
2016-04-20 10:39:48 +0200

    Finalisation cours chapitre 6

--- chapitre_6/js/cours.js
+++ chapitre_6/js/cours.js
@@ -1,39 +1,24 @@
 var pseudoElt = document.getElementById("pseudo");
+pseudoElt.value = "MonPseudo";
+
+// Affiche d'un message contextuel pour la saisie du pseudo
 pseudoElt.addEventListener("focus", function () {
     document.getElementById("aidePseudo").textContent = "Entrez votre pseudo";
 });
+// Suppression du message contextuel pour la saisie du pseudo
 pseudoElt.addEventListener("blur", function (e) {
-    //console.log("Votre pseudo est " + e.target.value);
     document.getElementById("aidePseudo").textContent = "";
 });
 
+// Focus sur la zone de saisie du pseudo
 pseudoElt.focus();
 
-document.getElementById("mdp").addEventListener("input", function (e) {
-    var mdp = e.target.value;
-    var complexiteMdp = "faible";
-    if (mdp.length >= 8) {
-        complexiteMdp = "forte";
-    } else if (mdp.length >= 4) {
-        complexiteMdp = "moyenne";
-    }
-    document.getElementById("aideMdp").textContent = "Complexité : " + complexiteMdp;
-});
-
-document.getElementById("courriel").addEventListener("blur", function (e) {
-    var regexCourriel = /.+@.+\..+/; // /[a-zA-Z]+@[a-zA-Z]+\.(com|fr)/;
-    var validiteCourriel = "";
-    //if (e.target.value.indexOf("@") === -1) {
-    if (!regexCourriel.test(e.target.value)) {
-        validiteCourriel = "Adresse invalide";
-    }
-    document.getElementById("aideCourriel").textContent = validiteCourriel;
-});
-
+// Affichage de la demande de confirmation d'inscription
 document.getElementById("confirmation").addEventListener("change", function (e) {
     console.log("Demande de confirmation : " + e.target.checked);
 });
 
+// Affichage du type d'abonnement choisi
 var aboElts = document.getElementsByName("abonnement");
 for (var i = 0; i < aboElts.length; i++) {
     aboElts[i].addEventListener("change", function (e) {
@@ -41,11 +26,17 @@ for (var i = 0; i < aboElts.length; i++) {
     });
 }
 
+// Affichage du code de nationalité choisi
 document.getElementById("nationalite").addEventListener("change", function (e) {
     console.log("Code de nationalité : " + e.target.value);
 });
 
 var form = document.querySelector("form");
+/*console.log("Nombre de champs de saisie : " + form.elements.length); // Affiche 10
+console.log(form.elements[0].name); // Affiche "pseudo"
+console.log(form.elements.mdp.type); // Affiche "password"*/
+
+// Affiche de toutes les données saisies ou choisies
 form.addEventListener("submit", function (e) {
     var pseudo = form.elements.pseudo.value;
     var mdp = form.elements.mdp.value;
@@ -83,5 +74,41 @@ form.addEventListener("submit", function (e) {
     default:
         console.log("Erreur : code de nationalité non reconnu");
     }
-    e.preventDefault();
+    e.preventDefault(); // Annulation de l'envoi des données
 });
+
+// Vérification de la longueur du mot de passe saisi
+document.getElementById("mdp").addEventListener("input", function (e) {
+    var mdp = e.target.value; // Valeur saisie dans le champ mdp
+    var longueurMdp = "faible";
+    var couleurMsg = "red"; // Longueur faible => couleur rouge
+    if (mdp.length >= 8) {
+        longueurMdp = "suffisante";
+        couleurMsg = "green"; // Longueur suffisante => couleur verte
+    } else if (mdp.length >= 4) {
+        longueurMdp = "moyenne";
+        couleurMsg = "orange"; // Longueur moyenne => couleur orange
+    }
+    var aideMdpElt = document.getElementById("aideMdp");
+    aideMdpElt.textContent = "Longueur : " + longueurMdp; // Texte de l'aide
+    aideMdpElt.style.color = couleurMsg; // Couleur du texte de l'aide
+});
+
+// Contrôle du courriel en fin de saisie
+document.getElementById("courriel").addEventListener("blur", function (e) {
+    // Correspond à une chaîne de la forme xxx@yyy.zzz
+    var regexCourriel = /.+@.+\..+/;
+    var validiteCourriel = "";
+    //if (e.target.value.indexOf("@") === -1) { // Solution avec indexOf
+    // Le courriel saisi ne contient pas le caractère @
+    if (!regexCourriel.test(e.target.value)) {
+        validiteCourriel = "Adresse invalide";
+    }
+    document.getElementById("aideCourriel").textContent = validiteCourriel;
+});
+
+var regex = /@/; // La chaîne doit contenir le caractère @
+console.log(regex.test("")); // Affiche false
+console.log(regex.test("@")); // Affiche true
+console.log(regex.test("sophie&mail.fr")); // Affiche false
+console.log(regex.test("sophie@mail.fr")); // Affiche true
