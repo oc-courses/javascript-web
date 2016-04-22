462cb37154cab64c6c493560e0f910ce815af5a4
bpesquet <bpesquet@gmail.com>
2016-04-22 15:23:24 +0200

    Finalisation du cours

--- chapitre_7/js/cours.js
+++ chapitre_7/js/cours.js
@@ -1,37 +1,50 @@
 var compteurElt = document.getElementById("compteur");
 
+// Diminue le compteur jusqu'à 0
 function diminuerCompteur() {
+    // Conversion en nombre du texte du compteur
     var compteur = Number(compteurElt.textContent);
     if (compteur > 1) {
         compteurElt.textContent = compteur - 1;
     } else {
-        clearInterval(timer);
+        // Annule l'exécution répétée
+        clearInterval(intervalId);
+        // Modifie le titre de la page
         var titre = document.getElementById("titre");
         titre.textContent = "BOUM !!!";
+        // Modification du titre au bout de 2 secondes
         setTimeout(function () {
             titre.textContent = "Tout est cassé :(";
         }, 2000);
     }
 }
 
-// Appelle la fonction diminuerCompteur toutes les secondes
-var timer = setInterval(diminuerCompteur, 1000);
+// Appelle la fonction diminuerCompteur toutes les secondes (1000 millisecondes)
+var intervalId = setInterval(diminuerCompteur, 1000);
 
 var cadre = document.getElementById("cadre");
 var bloc = document.getElementById("bloc");
-var diametreBloc = parseFloat(getComputedStyle(bloc).width);
-var xMin = 0;
-var vitesse = 7;
+var vitesse = 7; // Valeur du déplacement en pixels
+// Conversion en nombre du diamètre du bloc (valeur de la forme "XXpx")
+var largeurBloc = parseFloat(getComputedStyle(bloc).width);
+var animationId = null; // Identifiant de l'animation
 
+// Déplace le bloc sur sa gauche jusqu'au bord du cadre
 function deplacerBloc() {
-    var idRequest = requestAnimationFrame(deplacerBloc);
-
+    // Conversion en nombre de la position gauche du bloc (valeur de la forme "XXpx")
     var xBloc = parseFloat(getComputedStyle(bloc).left);
+    // Conversion en nombre de la largeur du cadre (valeur de la forme "XXpx")
     var xMax = parseFloat(getComputedStyle(cadre).width);
-    if (xBloc + diametreBloc <= xMax) {
+    if (xBloc + largeurBloc <= xMax) { // Si le bloc n'est pas encore arrivé au bout du cadre
+        // Déplacement du bloc
         bloc.style.left = (xBloc + vitesse) + "px";
+        // Demande au navigateur d'appeler deplacerBloc dès que possible
+        animationId = requestAnimationFrame(deplacerBloc);
     } else {
-        cancelAnimationFrame(idRequest);
+        // Annulation de l'animation
+        cancelAnimationFrame(animationId);
     }
 }
-deplacerBloc();
+
+animationId = requestAnimationFrame(deplacerBloc); // Début de l'animation    
+
