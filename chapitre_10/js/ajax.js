679010989942f16b2ec67ebac30d61d74dae8911
bpesquet <bpesquet@gmail.com>
2016-05-05 23:09:46 +0200

    Début du chapitre 10

---
+++ chapitre_10/js/ajax.js
@@ -0,0 +1,42 @@
+// Exécute un appel AJAX GET
+// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
+
+// Exécute un appel AJAX POST
+// Prend en paramètres l'URL cible et la donnée à envoyer
+// Le 3ème paramètre indique si la donnée est au format JSON ou non
+function ajaxPost(url, data, isJson) {
+    var req = new XMLHttpRequest();
+    req.open("POST", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 400) {
+            // Affiche un message en cas d'échec de la requête
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    if (isJson) {
+        // Définit le contenu de la requête comme étant du JSON
+        req.setRequestHeader("Content-Type", "application/json");
+        // Transforme la donnée du JSON au texte avant l'envoi
+        data = JSON.stringify(data);
+    }
+    req.send(data);
+}
--- chapitre_10/js/cours.js
+++ chapitre_10/js/cours.js
@@ -1,88 +1,18 @@
-function getAsync(url, callback) {
-    var req = new XMLHttpRequest();
-    req.open("GET", url, true);
-    req.addEventListener("load", function () {
-        if (req.status < 400) {
-            callback(req.responseText);
-        } else {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    req.send(null);
-}
+var identite = new FormData();
+identite.append("login", "Bob");
+identite.append("password", "azerty");
+ajaxPost("http://localhost/javascript-web-srv/post_form.php", identite);
 
-function postJson(url, data) {
-    var req = new XMLHttpRequest();
-    req.open("POST", url, true);
-    req.addEventListener("load", function () {
-        if (req.status >= 400) {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    req.setRequestHeader("Content-Type", "application/json");
-    req.send(JSON.stringify(data));
-}
-
-function postForm(url, data) {
-    var req = new XMLHttpRequest();
-    req.open("POST", url, true);
-    req.addEventListener("load", function () {
-        if (req.status >= 400) {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    req.send(data);
-}
-
-var articlesElt = document.getElementById("articles");
-getAsync("http://localhost/javascript-web-srv/web/api/articles", function (texteReponse) {
-    var articles = JSON.parse(texteReponse);
-    articles.forEach(function (article) {
-        var titreElt = document.createElement("h2");
-        titreElt.textContent = article.title;
-        var contenuElt = document.createElement("p");
-        contenuElt.textContent = article.content;
-        articlesElt.appendChild(titreElt);
-        articlesElt.appendChild(contenuElt);
-    });
-});
-
-var nouvelArticle = {
-    title: "JavaScript c'est top !",
-    content: "Cet article a été créé en JavaScript."
-};
-/*var request = new XMLHttpRequest();
-request.open('POST', 'http://localhost/javascript-web-srv/web/api/article', true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify(nouvelArticle));*/
-postJson("http://localhost/javascript-web-srv/web/api/article", nouvelArticle);
-
-document.querySelector("form").addEventListener("submit", function (e) {
+var form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
     e.preventDefault();
-    var data = new FormData(e.target);
-    postForm("http://localhost/javascript-web-srv/web/api/article", data);
+    var data = new FormData(form);
+    ajaxPost("http://localhost/javascript-web-srv/post_form.php", data);
 });
 
-getAsync("https://www.data.gouv.fr/api/1/organizations/premier-ministre/", function (texteReponse) {
-    var reponse = JSON.parse(texteReponse);
-    console.log(reponse.description);
-    console.log(reponse.logo);
-});
-
-// API key d'Emily Reese
-getAsync("http://api.wunderground.com/api/6ae045e189524fd2/conditions/q/France/Lyon.json", function (texteReponse) {
-    //console.log(texteReponse);
-});
-
-getAsync("https://api.github.com/users/bpesquet", function (texteReponse) {
-    //console.log(texteReponse);
-});
+var film = {
+    titre: "Zootopie",
+    annee: "2016",
+    realisateur: "Byron Howard et Rich Moore"
+};
+ajaxPost("http://localhost/javascript-web-srv/post_json.php", film, true);
