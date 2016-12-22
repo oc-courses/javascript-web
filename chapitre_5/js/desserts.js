7e3a708a0ecfd1afd7744ffc01c7108042f8f713
Say3569 <saysavong@gmail.com>
2016-12-22 12:37:27 +0100

    Suppresion du second addEventListener
    Bonjour, j'ai testé et cela semble fonctionner correctement.
    Pourquoi avoir mis une répétition du addEventListener?
    Je débute. Merci de regarder.

--- chapitre_5/js/desserts.js
+++ chapitre_5/js/desserts.js
@@ -7,10 +7,6 @@ document.querySelector("button").addEventListener("click", function () {
 
     var dessertElt = document.createElement("li");
     dessertElt.textContent = nomDessert;
-    dessertElt.addEventListener("click", function (e) {
-        var nouveauNom = prompt("Modifiez le nom du dessert :", e.target.textContent);
-        e.target.textContent = nouveauNom;
-    });
 
     document.getElementById("desserts").appendChild(dessertElt);
 });
