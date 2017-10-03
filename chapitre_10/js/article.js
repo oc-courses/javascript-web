19994315ed228f6a5ed9d67dbd08f506c9dcedeb
bpesquet <bpesquet@gmail.com>
2017-10-03 22:24:41 +0200

    Switch Heroku requests to HTTPS

--- chapitre_10/js/article.js
+++ chapitre_10/js/article.js
@@ -1,10 +1,12 @@
 var form = document.querySelector("form");
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    var article = new FormData(form);
-    ajaxPost("http://oc-jswebsrv.herokuapp.com/article", article, function (reponse) {
-        var messageElt = document.createElement("p");
-        messageElt.textContent = "L'article a bien été ajouté.";
-        document.getElementById("resultat").appendChild(messageElt);
-    });
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  var article = new FormData(form);
+  ajaxPost("https://oc-jswebsrv.herokuapp.com/article", article, function(
+    reponse
+  ) {
+    var messageElt = document.createElement("p");
+    messageElt.textContent = "L'article a bien été ajouté.";
+    document.getElementById("resultat").appendChild(messageElt);
+  });
 });
--- chapitre_10/js/temoignage.js
+++ chapitre_10/js/temoignage.js
@@ -1,13 +1,18 @@
-document.querySelector("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    var temoignage = {
-        pseudo: e.target.elements.pseudo.value,
-        evaluation: e.target.elements.evaluation.value,
-        message: e.target.elements.message.value,
-    };
-    ajaxPost("http://oc-jswebsrv.herokuapp.com/api/temoignage", temoignage, function (reponse) {
-        var messageElt = document.createElement("p");
-        messageElt.textContent = "Témoignage ajouté.";
-        document.getElementById("resultat").appendChild(messageElt);
-    }, true);
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  var temoignage = {
+    pseudo: e.target.elements.pseudo.value,
+    evaluation: e.target.elements.evaluation.value,
+    message: e.target.elements.message.value
+  };
+  ajaxPost(
+    "https://oc-jswebsrv.herokuapp.com/api/temoignage",
+    temoignage,
+    function(reponse) {
+      var messageElt = document.createElement("p");
+      messageElt.textContent = "Témoignage ajouté.";
+      document.getElementById("resultat").appendChild(messageElt);
+    },
+    true
+  );
 });
